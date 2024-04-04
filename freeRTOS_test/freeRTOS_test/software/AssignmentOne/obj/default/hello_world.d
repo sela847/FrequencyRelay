3123ada38240d1e73c67c741a94c1edb39a50064
Shaaran Elango <sela847@UoA.auckland.ac.nz>
2024-04-04 19:55:16 +1300

    More changes, got the keyboard kinda working

---
+++ freeRTOS_test/freeRTOS_test/software/AssignmentOne/obj/default/hello_world.d
@@ -0,0 +1,67 @@
+obj/default/hello_world.o: hello_world.c ../AssignmentOne_bsp/system.h \
+ ../AssignmentOne_bsp/linker.h \
+ ../AssignmentOne_bsp//HAL/inc/sys/alt_irq.h \
+ ../AssignmentOne_bsp//HAL/inc/nios2.h \
+ ../AssignmentOne_bsp//HAL/inc/alt_types.h \
+ ../AssignmentOne_bsp//HAL/inc/priv/alt_legacy_irq.h \
+ ../AssignmentOne_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../AssignmentOne_bsp//HAL/inc/io.h \
+ ../AssignmentOne_bsp//HAL/inc/alt_types.h FreeRTOS/FreeRTOS.h \
+ FreeRTOS/FreeRTOSConfig.h FreeRTOS/projdefs.h FreeRTOS/portable.h \
+ FreeRTOS/deprecated_definitions.h FreeRTOS/portmacro.h \
+ FreeRTOS/mpu_wrappers.h FreeRTOS/task.h FreeRTOS/list.h FreeRTOS/queue.h \
+ ../AssignmentOne_bsp//drivers/inc/altera_up_avalon_ps2.h \
+ ../AssignmentOne_bsp//HAL/inc/sys/alt_dev.h \
+ ../AssignmentOne_bsp//HAL/inc/sys/alt_llist.h \
+ ../AssignmentOne_bsp//HAL/inc/priv/alt_dev_llist.h FreeRTOS/semphr.h \
+ FreeRTOS/queue.h
+
+../AssignmentOne_bsp/system.h:
+
+../AssignmentOne_bsp/linker.h:
+
+../AssignmentOne_bsp//HAL/inc/sys/alt_irq.h:
+
+../AssignmentOne_bsp//HAL/inc/nios2.h:
+
+../AssignmentOne_bsp//HAL/inc/alt_types.h:
+
+../AssignmentOne_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+../AssignmentOne_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../AssignmentOne_bsp//HAL/inc/io.h:
+
+../AssignmentOne_bsp//HAL/inc/alt_types.h:
+
+FreeRTOS/FreeRTOS.h:
+
+FreeRTOS/FreeRTOSConfig.h:
+
+FreeRTOS/projdefs.h:
+
+FreeRTOS/portable.h:
+
+FreeRTOS/deprecated_definitions.h:
+
+FreeRTOS/portmacro.h:
+
+FreeRTOS/mpu_wrappers.h:
+
+FreeRTOS/task.h:
+
+FreeRTOS/list.h:
+
+FreeRTOS/queue.h:
+
+../AssignmentOne_bsp//drivers/inc/altera_up_avalon_ps2.h:
+
+../AssignmentOne_bsp//HAL/inc/sys/alt_dev.h:
+
+../AssignmentOne_bsp//HAL/inc/sys/alt_llist.h:
+
+../AssignmentOne_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+FreeRTOS/semphr.h:
+
+FreeRTOS/queue.h:
